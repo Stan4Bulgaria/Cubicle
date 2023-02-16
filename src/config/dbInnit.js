f24ec6a2cc0931c5c20cf264a605a398d3eb6e77
Stan4Bulgaria <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-02-16 17:39:36 +0200

    Delete src directory

--- src/config/dbInnit.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('./config');
-async function dbInnit(){
-    mongoose.set('strictQuery', false);
-    await mongoose.connect(config.DB_URI);
-    console.log('Db connected');
-}
-module.exports = dbInnit;
