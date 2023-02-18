37cba2c173f9e8663fddd6987f6ce979f13e5483
Stan4Bulgaria <117578267+Stan4Bulgaria@users.noreply.github.com>
2023-02-18 17:22:28 +0200

    Delete src directory

--- src/manager/cubeManager.js
+++
@@ -1,7 +0,0 @@
-const Cube = require('../models/Cube');
-exports.getOneCube = async (cubeId) => {
-    const cube = await Cube.findById(cubeId).lean();
-    return cube;
-}
-exports.update = async (cubeId, data) => await Cube.findByIdAndUpdate(cubeId, data, { runValidators: true })
-exports.delete = async (cubeId) => await Cube.findByIdAndDelete(cubeId);
--- src/utils/jsonwebtoken.js
+++
@@ -1,7 +0,0 @@
-const util = require('util');
-const jwtCallback = require('jsonwebtoken');
-const jwt = {
-    sign: util.promisify(jwtCallback.sign),
-    verify: util.promisify(jwtCallback.verify)
-};
-module.exports = jwt;
